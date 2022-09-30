d087660b753b3e63b19ceee854d03df0f735f76e
felipelastra26 <108039983+felipelastra26@users.noreply.github.com>
2022-09-29 22:36:53 -0500

    Update PyPoll_Challenge.py

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -7,7 +7,7 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("/Users/felipelastra/Library/CloudStorage/OneDrive-Personal/Analytics/Module3/Election_Analysis//Resources/election_results.csv")
+file_to_load = os.path.join("/Resources/election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
